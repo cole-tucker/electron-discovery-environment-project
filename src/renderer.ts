505fb8fbbaaba554cf593e50da0ce4d98a74271c
Cole Tucker <coletucker@email.arizona.edu>
2019-09-11 15:24:47 -0700

    Init get call

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+import request = require('request');
+
+var username = document.getElementById('username');
+var password = document.getElementById('password');
+
+var getToken = request.get('https://de.cyverse.org/terrain/token', {
+    'auth' : {
+        'user' : username,
+        'pass' : password
+    }
+});
