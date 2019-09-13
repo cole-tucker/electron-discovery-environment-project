2ab503a9e0163d5aa7e1641bc4e772aede0112d9
Cole Tucker <coletucker@email.arizona.edu>
2019-09-13 12:10:47 -0700

    request and package.json edit

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,14 +1,18 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-import request = require('request');
-
 var username = document.getElementById('username');
 var password = document.getElementById('password');
+var request = require('request');
 
-var getToken = request.get('https://de.cyverse.org/terrain/token', {
-    'auth' : {
-        'user' : username,
-        'pass' : password
+request('https://de.cyverse.org/terrain/token', {
+    'user': {
+        'username': username,
+        'password': password
     }
 });
+
+// console.log(getToken)
+
+// var token = getToken.json()['access_token'];
+// var auth_headers = {"Authorization": "Bearer " + token};
