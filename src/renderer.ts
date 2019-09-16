856dd75cd6a83f22f972687523281490bc494b4d
Cole Tucker <coletucker@email.arizona.edu>
2019-09-16 15:03:39 -0700

    Backend for login, gitignore update

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,18 +1,34 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var username = document.getElementById('username');
-var password = document.getElementById('password');
-var request = require('request');
 
-request('https://de.cyverse.org/terrain/token', {
-    'user': {
-        'username': username,
-        'password': password
-    }
-});
+let $ = require('jquery');
+let request = require('request');
+var store = require('data-store')({path: process.cwd() + '/token.json'});
 
-// console.log(getToken)
+var url = 'https://de.cyverse.org/terrain/token'
 
-// var token = getToken.json()['access_token'];
-// var auth_headers = {"Authorization": "Bearer " + token};
+$('#loginBtn').on("click", () => {
+    var username = ( < HTMLInputElement > document.getElementById('username')).value;
+    var password = ( < HTMLInputElement > document.getElementById('password')).value;
+
+    var auth = "Basic " + Buffer.from(username + ":" + password).toString("base64");
+
+    const req = request({
+            url: url,
+            headers: {
+                'Authorization': auth
+            },
+        },
+        function (req: any, res: any, body: any) {
+            var jsonObject = JSON.parse(body)
+            store.set('token', jsonObject['access_token'])
+            if (jsonObject['status'] === 401) {
+                console.log(jsonObject['error'])
+            } else {
+                console.log('Login Success')
+                $('#loginModal').modal('toggle');
+            }
+        }
+    )
+})
