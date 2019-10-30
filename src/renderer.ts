130f752c38d6fbb288dabdbb48f632a9a8215d1b
Cole Tucker <coletucker@email.arizona.edu>
2019-10-30 16:25:25 -0700

    Init for show app functionality

--- src/renderer.ts
+++ src/renderer.ts
@@ -50,19 +50,96 @@ $('#searchBtn').on('click', () => {
         $('#browse-apps').empty()
         request(options, function(err: any, res: any, body: any){
             var appsJson = JSON.parse(body);
+            console.log(body)
     
             $('#browse-apps').append('<h2>App Search Results</h2>')
             for (let i = 0; i < appsJson['apps'].length; i++) {
                 const app = appsJson['apps'][i];
                 if (app['name'].includes(query_params)) {
+                    var datatext:String = "data-system="+app['system_id']+" data-id="+app['id']
                     $('#browse-apps').append('<div class="card border col-5 mt-1" id="app-card-'+ i + '">')
                     $('#app-card-'+ i).append('<div class="card-body" id="app-card-body-' + i +'">')
                     $('#app-card-body-' + i).append('<h4 class="card-title">' + app['name'] + '</h4>')
                     $('#app-card-body-' + i).append('<p class="card-text">' + app['description'] + '</p>')
-                    $('#app-card-body-' + i).append('<button type="button" class="btn btn-primary center showApp" id="app-select-' + i + '">Select</button>')
+                    $('#app-card-body-' + i).append('<button type="button" '+ datatext +' class="btn btn-primary center showApp viewapp" id="app-select-' + i + '">Select</button>')
                 }
             }
             $('#browse-apps').show()
+
+            $('.showApp').on('click', () => {
+                console.log()
+            })
         })
     }
 });
+
+$(document).on("click", ".viewapp",
+    function (event:any) {
+        var elem:HTMLElement = event.target
+        var system_id:String = $(elem).data('system')
+        var id:String = $(elem).data('id')
+        applyAppTemplate(elem)
+    }
+);
+
+var stored_groups_length = -1;
+var stored_app_length = new Array()
+var stored_app_id = ""
+var stored_system_id = ""
+
+var applyAppTemplate = function (elem:any) {
+    const options = {
+        url: 'https://de.cyverse.org/terrain/apps/' + system_id + '/' + app_id,
+        headers: {
+            "Authorization": "Bearer " + store.get('token')
+        }
+    }
+
+    var jelm = $(elem);//convert to jQuery Element
+    stored_app_id = jelm.data("id");
+    stored_system_id = jelm.data("system");
+    var app_id = encodeURI(jelm.data("id"));
+    var system_id = encodeURI(jelm.data("system"))
+
+    $('#browse-apps').hide()
+    $('#show-app').show()
+
+    fetch('https://de.cyverse.org/terrain/apps/' + system_id + '/' + app_id, options)
+        .then(response => response.json())
+        .then(data => {
+            var groups = data.groups;
+            var inputs = ``
+            stored_groups_length = groups.length;
+            for (var i = 0; i < groups.length; i++) {
+                var paramaters = groups[i].parameters;
+                stored_app_length.push(paramaters.length);
+                for (var j = 0; j < paramaters.length; j++) {
+                    var param = paramaters[j];
+                    var new_input =
+                    `
+                    <div class="form-group">
+                    <label for="input${i}${j}">${param.label}</label>
+                    <input data-id="${param.id}" type="text" class="form-control" id="input${i}${j}" aria-describedby="helpme${i}${j}" placeholder="...">
+                    <small id="helpme${i}${j}" class="form-text text-muted">${param.description}</small>
+                    </div>
+                    `
+
+                    inputs += new_input;
+                }
+            }
+            var app_html =
+            `
+            <h1> ${jelm.data("name")} </h1>
+            <sub> If you're using the DE Word Count App try using this as the File Input! </sub>
+            <br/>
+            <sub> <span style="background-color:lightgray"> /iplant/home/shared/workshop_material/terrain_intro/essay.txt </span> </sub>
+            <hr/>
+            <form>
+            ` + inputs + `
+                <button id="submit-app" class="btn btn-primary">Submit</button>
+            </form>
+            `
+            $('#show-app').append(app_html);
+            // hideLoader();
+        });
+}
